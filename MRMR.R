632abdc58a4e1cfa8b5601d22aecc251a3997835
ISLAH-Hamza <67807772+ISLAH-Hamza@users.noreply.github.com>
2022-05-31 11:34:29 +0100

    using lapply

--- MRMR.R
+++ MRMR.R
@@ -1,41 +1,41 @@
+
 MRMR<-function(Featurs,target,k){
-  
   # The notations used
   # Featurs : Independent variables. !! Must not contain target variables
   # target : The variables to predict 
   # k : The number of variables to select
   # S : returned subset of selected features
-  # I : The Mutual Information Matrix
+  # I : The Mutual Information Matrix 
 
+  if(k>p){ return(-1) }  
+  if(k==p){return(seq(1:p))}
   
   #initialization
   p=ncol(Featurs)
   I=matrix(nrow = 2,ncol=p)
-  S=c()
-
-  if(k>p){ return(-1) }  
-  if(k==p){return(seq(1:p))}
-  for(i in 1:p){
-    I[2,i]=infotheo::mutinformation(Featurs[i],target)
-    I[1,i]=i
-  } 
-  
-  index=which.max(I[2,])
-  S=c(S,index) 
-  I=I[,-index]
-  
+  S=rep(NA,k)
   m=1
+  
+  #calculte the matrix of mutuel information 
+  I<-unlist(lapply(1:p, function(x){infotheo::mutinformation(Featurs[x],target) }))
+  I<-rbind(1:p,I)
+  
+  # Variables selections
+  index=which.max(I[2,])
+  S[1]=index
+  I=I[,-index]
   while(m < k){
+     m=m+1
      R=c()  
      for(i in 1:ncol(I)){
-       s=0
-       for(j in S){ s=s+infotheo::mutinformation(Featurs[j],Featurs[ I[1,i] ]) } 
-       R[i]=I[2,i]-s/length(S)
+       s=unlist(lapply(1:m, function(j){ infotheo::mutinformation(Featurs[j],Featurs[ I[1,i] ])})) 
+       R[i]=I[2,i]-sum(s)/length(S)
      }
+    
      index=which.max(R)
-     S=c(S,I[1,index])
+     S[m]=I[1,index]
      I=I[,-index]
-     m=m+1
+     
   }
   
   return(S)
